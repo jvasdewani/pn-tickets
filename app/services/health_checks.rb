d48140fed385b78139ff14f9081468e4c895738c
Qu@ntum21 <jvasdewani@gmail.com>
2014-03-03 18:45:09 +0530

    Added health check cron as a service

---
+++ app/services/health_checks.rb
@@ -0,0 +1,41 @@
+class HealthChecks
+  include Sidekiq::Worker
+
+  def perform
+    hc_start_date = Time.now.beginning_of_day + 1.week
+
+    @healthchecks = Client.where("health_check_date <= ?", hc_start_date)
+
+    @healthchecks.each do |hc|
+      client = Client.find(hc.id)
+      if hc.health_check_department.present?
+        department = Department.find(hc.health_check_department)
+      else
+        department = Department.first
+      end
+
+      if hc.health_check_person.present?
+        person = Person.find(hc.health_check_person)
+      else
+        person = Person.first
+      end
+
+      comment = Comment.new
+      comment.person = person
+      comment._ns = 40
+      comment._np = 20
+
+      issue = Issue.new
+      issue.subject = "Health Check (#{client.check_date})"
+      issue.person = person
+      issue.department = department
+      issue.client = client
+      issue.comments << comment
+      issue.save
+
+      new_date = client.health_check_date + 3.months
+      client.update_attribute :health_check_date, new_date
+    end
+  end
+end
+
